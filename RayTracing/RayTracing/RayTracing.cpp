e0307b788a2b425add29a59707000827303c39ce
Daniel Lott <daniel.lott@projectpve.com>
2018-08-14 17:50:30 -0500

    Chapter 2 vec3

---
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() 
+{
+	int NumberOfX = 200;
+	int NumberOfY = 100;
+	std::ofstream out("out.ppm");
+	std::streambuf *coutbuf = std::cout.rdbuf();
+	std::cout.rdbuf(out.rdbuf());
+	std::cout << "P3\n" << NumberOfX << " " << NumberOfY << "\n255\n";
+	for (int j = NumberOfY - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < NumberOfX; i++)
+		{
+			float r = float(i) / float(NumberOfX);
+			float g = float(j) / float(NumberOfY);
+			float b = 0.2;
+			int ir = int(255.99*r);
+			int ig = int(255.99*g);
+			int ib = int(255.99*b);
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+}
