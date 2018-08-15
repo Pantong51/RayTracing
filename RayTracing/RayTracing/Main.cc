6f34b2076f781a8b9d41c099b0e121098bbb02f1
Daniel Lott <daniel.lott@projectpve.com>
2018-08-15 14:34:46 -0500

    Bug Fixed - conveted bool to float method

--- RayTracing/RayTracing/Main.cc
+++ RayTracing/RayTracing/Main.cc
@@ -4,17 +4,13 @@
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r)
+float hit_sphere(const vec3& center, float radius, const ray& r)
 {
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0 * dot(oc, r.direction());
 	float c = dot(oc, oc) - radius*radius;
-
 	float discriminant = b*b - 4*a*c;
-	printf("%b", signbit(discriminant));
-	//return (discriminant > 0);
-	
 	if (discriminant < 0)
 	{
 		return -1.0;
@@ -27,11 +23,6 @@ bool hit_sphere(const vec3& center, float radius, const ray& r)
 
 vec3 color(const ray& r)
 {
-	/*
-	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
-	{
-		return vec3(1, 0, 0);
-	}*/
 	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
 	if (t > 0.0)
 	{
