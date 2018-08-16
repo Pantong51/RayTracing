51554ff844bdabe8d3ed992a8909596553a637e1
Daniel Lott <daniel.lott@projectpve.com>
2018-08-16 09:35:52 -0500

    Bug in ch5 - no sphere drawing

--- RayTracing/RayTracing/Main.cc
+++ RayTracing/RayTracing/Main.cc
@@ -1,43 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "vec3.h"
-#include "ray.h"
+#include "sphere.h"
+#include "hitablelist.h"
+#include "float.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r)
+vec3 color(const ray& r, hitable *world)
 {
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius*radius;
-	float discriminant = b*b - 4*a*c;
-	if (discriminant < 0)
+	hit_record rec;
+	if (world->hit(r, 0.001f, FLT_MAX, rec))
 	{
-		return -1.0;
+		return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
 	else
 	{
-		return (-b - sqrt(discriminant)) / (2.0*a);
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5f*(unit_direction.y() + 1.0f);
+		return (1.0f - t)*vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
 	}
-}
-
-vec3 color(const ray& r)
-{
-	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-	if (t > 0.0)
-	{
-		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-		return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
-	}
-	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5*(unit_direction.y() + 1.0);
-	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main()
 {
-	int NumberOfX = 1920;
-	int NumberOfY = 1080;
+	int NumberOfX = 200;
+	int NumberOfY = 100;
 	std::ofstream out("out.ppm");
 	std::streambuf *coutbuf = std::cout.rdbuf();
 	std::cout.rdbuf(out.rdbuf());
@@ -48,6 +34,11 @@ int main()
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
 
+	hitable *list[2];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	hitable *world = new hitable_list(list, 2);
+
 	for (int j = NumberOfY - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < NumberOfX; i++)
@@ -55,10 +46,14 @@ int main()
 			float u = float(i) / float(NumberOfX);
 			float v = float(j) / float(NumberOfY);
 			ray r(origin, lower_left_corner + (u * horizontal) + (v * vertical));
-			vec3 col = color(r);
+
+			vec3 p = r.point_at_parameter(2.0);
+			vec3 col = color(r, world);
+
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
+
 			std::cout << ir << " " << ig << " " << ib << "\n";
 		}
 	}
